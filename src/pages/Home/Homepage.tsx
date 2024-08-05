c6c7d865ddf5b1afa12f76051b72f73456d82d4a
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-08-05 10:51:11 +0200

    add homepage custom message

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -9,14 +9,11 @@ export default function Homepage() {
         <div className="flex flex-col">
           <h1 className="text-white mx-auto font-bold py-10">Manag'Event</h1>
           <p className="text-justify text-white sm:w-10/12 sm:mx-auto  md:w-7/12 md:mx-auto lg:w-5/12 lg:mx-auto xl:w-4/12 xl:mx-auto py-2 p-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            neque in vel quod fuga illo sequi voluptate et voluptatibus dolor
-            temporibus distinctio, expedita nam, eum soluta. Quaerat cupiditate,
-            corporis quod iure pariatur sed animi quam aliquam, delectus esse
-            assumenda incidunt quos voluptates in accusamus, nesciunt voluptate?
-            Tenetur nobis fuga sapiente tempora similique quos odio hic
-            voluptatibus debitis dicta corporis in, soluta, ut nisi beatae
-            pariatur iusto sint omnis quam, aut autem! !
+            Bienvenue sur Manag'Event, notre application dédiée à la célébration
+            de la diversité des événements et des talents bénévoles. Chacune de
+            ces occasions, qu'elles soient culturelles, caritatives, sportives
+            ou artistiques, est une opportunité unique de rassembler nos forces
+            et nos passions pour créer des expériences mémorables et uniques.
           </p>
         </div>
         <div className="mb-5 mt-5">
